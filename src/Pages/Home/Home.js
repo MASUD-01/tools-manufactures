6ae310e493d951ee90e8c98698907a91e3135a00
MASUD-01 <masudrana96141@gmail.com>
2022-05-21 23:32:11 +0600

    add a navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+
+const Home = () => {
+    return (
+        <div className=''>
+            <Navbar></Navbar>
+
+        </div>
+    );
+};
+
+export default Home;
